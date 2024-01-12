ccc2d4ff457c74fa1f18f34dd0bbef951ef8fbee
Youssef Mohammed <49940573+YussefMo@users.noreply.github.com>
2024-01-12 14:53:46 +0200

    Add files via upload

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [['Year','umich.edu','indiana.edu','ucdavis.edu','berkeley.edu','mac.com','uct.ac.za','gmail.com','cam.ac.uk','yale.edu','virginia.edu'],
+[2005,57,12,11,12,12,14,10,7,5,9],
+[2006,255,69,65,64,56,51,52,53,53,44]
+];
